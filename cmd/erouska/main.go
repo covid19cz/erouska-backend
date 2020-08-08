59ce444919a930a5782b6c67d728c777b3e4b7c9
Marek Bartík <bartimar6@gmail.com>
2020-08-08 18:12:51 +0200

    cleanup

--- cmd/erouska/main.go
+++ cmd/erouska/main.go
@@ -1,13 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"github.com/covid19cz/erouska-backend/internal/logging"
-	"net/http"
-
 	"github.com/sethvargo/go-signalcontext"
-
-	server "github.com/covid19cz/erouska-backend/internal/httpserver"
 )
 
 func main() {
@@ -15,28 +10,6 @@ func main() {
 	ctx, done := signalcontext.OnInterrupt()
 	defer done()
 
-	logger := logging.FromContext(ctx)
-
-	var config server.Config = server.Config{Port: "8081"}
-
-	handler, err := server.NewHandler(ctx, &config)
-
-	if err != nil {
-		logger.Errorf("publish.NewHandler: %w", err)
-	}
-
-	mux := http.NewServeMux()
-	mux.Handle("/", handler)
-
-	srv, err := server.NewServer(ctx, &config)
-	if err != nil {
-		fmt.Errorf("server.New: %w", err)
-	}
-	logger.Infof("listening on :%s", config.Port)
-
-	if err := srv.ServeHTTPHandler(ctx, mux); err != nil {
-		logger := logging.FromContext(ctx)
-		logger.Fatal(err)
-	}
+	_ = logging.FromContext(ctx)
 
 }
