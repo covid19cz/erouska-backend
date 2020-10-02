510662e7ce41652c885230272d2ea7ed3a43cad8
Jan Kolena <jendakolena@gmail.com>
2020-10-02 11:17:04 +0200

    Run downloadcovid function every hour (#100)

--- terraform/modules/erouska/coviddata.tf
+++ terraform/modules/erouska/coviddata.tf
@@ -68,7 +68,7 @@ resource "google_project_iam_member" "downloadcovid-invoker" {
 resource "google_cloud_scheduler_job" "downloadcovid-worker" {
   name             = "downloadcovid-worker"
   region           = var.cloudscheduler_location
-  schedule         = "0 3 * * *"
+  schedule         = "0 * * * *"
   time_zone        = "Europe/Prague"
   attempt_deadline = "600s"
 
