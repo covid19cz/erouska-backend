cc05c69fa2843eb46528fdc1b91d2dd7ec6e1ea6
Matěj Jehlička <jehlicka@mjehlicka.eu>
2020-10-08 11:24:59 +0200

    Comunnication with database (EFGS - Proxy keys).

--- go.mod
+++ go.mod
@@ -7,8 +7,11 @@ require (
 	cloud.google.com/go/firestore v1.2.0
 	cloud.google.com/go/pubsub v1.3.1
 	firebase.google.com/go v3.13.0+incompatible
+	github.com/GoogleCloudPlatform/cloudsql-proxy v1.18.0
 	github.com/avast/retry-go v2.6.0+incompatible
+	github.com/go-pg/pg/v10 v10.3.2
 	github.com/golang/gddo v0.0.0-20200715224205-051695c33a3f
+	github.com/golang/protobuf v1.4.2
 	github.com/google/exposure-notifications-server v0.7.0
 	github.com/google/go-cmp v0.5.2
 	github.com/sethvargo/go-envconfig v0.3.1
@@ -17,6 +20,7 @@ require (
 	go.uber.org/zap v1.16.0
 	google.golang.org/genproto v0.0.0-20200901141002-b3bf27a9dbd1
 	google.golang.org/grpc v1.31.1
+	google.golang.org/protobuf v1.25.0
 	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 	gopkg.in/go-playground/validator.v9 v9.31.0
 )
---
+++ internal/functions/efgs/database.go
@@ -0,0 +1,119 @@
+package efgs
+
+import (
+	"context"
+	"github.com/covid19cz/erouska-backend/internal/logging"
+	"net"
+
+	"github.com/GoogleCloudPlatform/cloudsql-proxy/proxy/proxy"
+	"github.com/covid19cz/erouska-backend/internal/secrets"
+	"github.com/go-pg/pg/v10"
+	"github.com/go-pg/pg/v10/orm"
+)
+
+//Singleton database struct.
+var database Database
+
+//Database Contains database connection pool.
+type Database struct {
+	connection *pg.DB
+}
+
+//Create new database connection pool. Credentials must be specified in secret manager.
+func init() {
+	ctx := context.Background()
+	logger := logging.FromContext(ctx)
+	secretsClient := secrets.Client{}
+
+	efgsDatabaseName, err := secretsClient.Get("efgs-database-name")
+	if err != nil {
+		logger.Fatalf("Connection to secret manager failed: %s", err)
+		return
+	}
+	efgsDatabasePassword, err := secretsClient.Get("efgs-database-password")
+	if err != nil {
+		logger.Fatalf("Connection to secret manager failed: %s", err)
+		return
+	}
+	efgsDatabaseUser, err := secretsClient.Get("efgs-database-login")
+	if err != nil {
+		logger.Fatalf("Connection to secret manager failed: %s", err)
+		return
+	}
+	efgsDatabaseConnectionName, err := secretsClient.Get("efgs-database-connection-name")
+	if err != nil {
+		logger.Fatalf("Connection to secret manager failed: %s", err)
+		return
+	}
+
+	database.connection = pg.Connect(&pg.Options{
+		Dialer: func(ctx context.Context, network, addr string) (net.Conn, error) {
+			return proxy.Dial(string(efgsDatabaseConnectionName))
+		},
+		User:     string(efgsDatabaseUser),
+		Password: string(efgsDatabasePassword),
+		Database: string(efgsDatabaseName),
+	})
+
+	if err := database.createSchema(); err != nil {
+		logger.Fatalf("Error while creating DB schema: %s", err)
+		return
+	}
+}
+
+//PersistDiagnosisKeys Save array of DiagnosisKey to database
+func (db Database) PersistDiagnosisKeys(keys []*DiagnosisKey) error {
+	connection := db.connection.Conn()
+	defer connection.Close()
+
+	_, err := connection.Model(keys).Insert()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+//GetDiagnosisKeys Get keys from database that are not yet in EFGS and are older than date in parameter.
+func (db Database) GetDiagnosisKeys(date string) ([]*DiagnosisKeyWrapper, error) {
+	connection := db.connection.Conn()
+	defer connection.Close()
+
+	var keys []*DiagnosisKeyWrapper
+	if err := connection.Model(&keys).Where("created_at >= ?", date).Select(); err != nil {
+		return nil, err
+	}
+	return keys, nil
+}
+
+//RemoveDiagnosisKey Remove array of DiagnosisKeyWrapper from database.
+func (db Database) RemoveDiagnosisKey(keys []*DiagnosisKeyWrapper) error {
+	connection := db.connection.Conn()
+	defer connection.Close()
+
+	_, err := connection.Model(keys).WherePK().Delete()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (db Database) createSchema() error {
+	connection := db.connection.Conn()
+	defer connection.Close()
+
+	models := []interface{}{
+		(*DiagnosisKeyWrapper)(nil),
+	}
+
+	for _, model := range models {
+		err := connection.Model(model).CreateTable(&orm.CreateTableOptions{
+			IfNotExists: true,
+		})
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
