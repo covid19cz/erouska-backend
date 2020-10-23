d752ab2484d007d4a245761e11310d91b90c9450
Matěj Jehlička <jehlicka@mjehlicka.eu>
2020-10-23 16:22:56 +0200

    Remove old keys from EFGS upload batch

---
+++ internal/functions/efgs/cleanup-database.go
@@ -0,0 +1,40 @@
+package efgs
+
+import (
+	"fmt"
+	efgsdatabase "github.com/covid19cz/erouska-backend/internal/functions/efgs/database"
+	"github.com/covid19cz/erouska-backend/internal/logging"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+)
+
+//CleanupDatabase Remove old (more than EXPOSURE_KEYS_EXPIRATION days) keys from database.
+func CleanupDatabase(w http.ResponseWriter, r *http.Request) {
+	var ctx = r.Context()
+	logger := logging.FromContext(ctx).Named("efgs.CleanupDatabase")
+	keyExpiration, isSet := os.LookupEnv("EXPOSURE_KEYS_EXPIRATION")
+	if !isSet {
+		err := fmt.Errorf("EXPOSURE_KEYS_EXPIRATION must be set")
+		logger.Error(err)
+		sendErrorResponse(w, err)
+		return
+	}
+
+	keyValidityDays, err := strconv.Atoi(keyExpiration)
+	if err != nil {
+		logger.Errorf("Error converting key validity days to int: %s", err)
+		sendErrorResponse(w, err)
+		return
+	}
+	dateFrom := time.Now().AddDate(0, 0, -keyValidityDays).Format("2006-01-02")
+
+	if err := efgsdatabase.Database.RemoveOldKeys(dateFrom); err != nil {
+		logger.Errorf("Cleanup failed: %s", err)
+		sendErrorResponse(w, err)
+		return
+	}
+
+	logger.Infof("Database is cleared")
+}
