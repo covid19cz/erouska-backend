2b942a2ab609912ef953d1c16b7bb11564bd29f7
Marek Bartík <bartimar6@gmail.com>
2020-10-27 16:31:51 +0100

    infra: use atlantis module

--- terraform/ci/main.tf
+++ terraform/ci/main.tf
@@ -10,11 +10,22 @@ provider "google" {
   region  = var.region
 }
 
-terraform {
-  required_providers {
-    google      = "~> 3.32"
-    google-beta = "~> 3.32"
-    null        = "~> 2.1"
-    random      = "~> 2.3"
-  }
+module "atlantis" {
+  source = "git::https://github.com/pipetail/terraform-atlantis-gce.git?ref=v0.1.0"
+
+  region  = var.region
+  project = var.project
+
+  zone           = var.zone
+  instance_name  = var.instance_name
+  repo_allowlist = var.repo_allowlist
+  image          = var.image
+  webhook_secret = var.webhook_secret
+  github_token   = var.github_token
+  github_user    = var.github_user
+
+}
+
+output "atlantis_ip" {
+  value = module.atlantis.atlantis_ip
 }
