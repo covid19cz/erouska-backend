e5c1ecfc176118618bae17b45ae01292006df2ef
Jan Kolena <jendakolena@gmail.com>
2020-12-07 21:42:28 +0100

    DownloadMetrics: accept date param, download for specific date

--- internal/functions/metricsapi/download.go
+++ internal/functions/metricsapi/download.go
@@ -21,8 +21,22 @@ func DownloadMetrics(w http.ResponseWriter, r *http.Request) {
 	logger := logging.FromContext(ctx).Named("DownloadMetrics")
 
 	client := store.Client{}
+	date := time.Now()
 
-	var date = time.Now()
+	var req v1.DownloadMetricsRequest
+
+	if !httputils.DecodeJSONOrReportError(w, r, &req) {
+		return
+	}
+
+	if req.Date != "" {
+		parse, err := time.Parse("2006-01-02", req.Date)
+		if err == nil {
+			date = parse
+		} else {
+			logger.Debugf("Could not parse requested date, fallback to today: %v", err)
+		}
+	}
 
 	data, err := downloadMetrics(ctx, client, date)
 	if err != nil {
@@ -31,42 +45,22 @@ func DownloadMetrics(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	if data == nil {
+		http.Error(w, "Data not found", 404)
+		return
+	}
+
 	httputils.SendResponse(w, r, data)
 }
 
 func downloadMetrics(ctx context.Context, client store.Client, date time.Time) (*v1.DownloadMetricsResponse, error) {
-	logger := logging.FromContext(ctx).Named("downloadMetrics")
-
-	data, err := fetchMetrics(ctx, client, date)
-	if err != nil {
-		return nil, fmt.Errorf("Error while fetching data: %v", err)
-	}
-
-	// fallback if data are not yet ready
-	if data == nil {
-		logger.Debugf("Data for %v are not ready yet, fallback to yesterday", date)
-
-		var date = time.Now().Add(time.Hour * -24)
-
-		fallbackData, err := fetchMetrics(ctx, client, date)
-		if err != nil {
-			return nil, fmt.Errorf("Error while fetching data: %v", err)
-		}
-
-		data = fallbackData
-	}
-
-	return data, nil
-}
-
-func fetchMetrics(ctx context.Context, client store.Storer, date time.Time) (*v1.DownloadMetricsResponse, error) {
 	logger := logging.FromContext(ctx).Named("fetchMetrics")
 
-	logger.Debugf("Getting metrics data for %v", date.Format("02.01.2006"))
+	logger.Infof("Getting metrics data for %v", date.Format("02.01.2006"))
 
 	rec, err := client.Doc(constants.CollectionMetrics, date.Format("20060102")).Get(ctx)
 	if status.Code(err) == codes.NotFound {
-		logger.Debugf("Data for %v not found", date.Format("02.01.2006"))
+		logger.Warnf("Data for %v not found", date.Format("02.01.2006"))
 		return nil, nil
 	}
 	if err != nil {
