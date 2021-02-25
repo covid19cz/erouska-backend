4e14c4fe8f7edcdb1b3245c994d18f49ab072dad
Marek Bartík <bartimar6@gmail.com>
2021-02-25 21:41:22 +0100

    feat: bump KS dev to 0.22.1

--- terraform/dev/key-server/main.tf
+++ terraform/dev/key-server/main.tf
@@ -11,7 +11,7 @@ provider "google" {
 }
 
 module "en" {
-  source = "git::https://github.com/google/exposure-notifications-server.git//terraform?ref=d51abd8"
+  source = "git::https://github.com/google/exposure-notifications-server.git//terraform?ref=v0.22.1"
 
   project = var.project
   region  = var.region
@@ -77,7 +77,7 @@ module "en" {
 
 module "cdn" {
 
-  source  = "../../modules/cdn"
+  source  = "../../modules/cdn-bucket"
   project = var.project
   region  = var.region
 
